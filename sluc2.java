558febc60d76950a780b5701c5fe6a9babbfb1db
madhu179 <madhubabu232343@gmail.com>
2020-09-22 22:08:02 +0530

    Adding SL UC2

---
+++ sluc2.java
@@ -0,0 +1,12 @@
+public class sluc2
+{
+	public static void main(String[] args)
+	{
+	int pos = 0;
+	int dice_roll = 0;
+	dice_roll = 1 + (int)(Math.random() * ((6 - 1) + 1));
+	System.out.println("Start Position is : "+ pos);
+	System.out.println("Dice Roll is : "+dice_roll);
+	}
+}
+
